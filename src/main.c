d3b0ddf8885365516020b4c4345adc5f8cf6fe08
dasilvar <remi.dasilva@edu.esiee.fr>
2023-12-26 11:56:44 +0800

    feat: add tree creation

--- src/main.c
+++ src/main.c
@@ -1,10 +1,7 @@
 #include "ui.h"
 
 int main(){
-    Tree* fileSystem = tree_new();
-    Node* root = node_new("./", NULL);
+    ui_mainMenu();
 
-    ui_mainMenu(fileSystem);
-    
     return 0;
 }
